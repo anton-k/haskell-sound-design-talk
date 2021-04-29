cb731f8428b5035cdaaa8d4244efd6b2a2631b1e
anton-k <anton.kholomiov@gmail.com>
2021-04-30 02:11:31 +0300

    upd changes

--- 02-music-algebra/examples/Utils.hs
+++ 02-music-algebra/examples/Utils.hs
@@ -3,11 +3,12 @@ module Utils where
 
 import Csound.Base
 
+
 -- | Euclidean beats
 --
 -- > euc totalLength initDelay durations scores
-euc :: Double -> Double -> [Double] -> [Sco a] -> Sco a
-euc len delTime durs scos =
+euclid :: Double -> Double -> [Double] -> [Sco a] -> Sco a
+euclid len delTime durs scos =
   go (delTime, rest (sig $ double delTime)) $ zip (cycle durs) (cycle scos)
   where
     go :: (Double, Sco a) -> [(Double, Sco  a)] -> Sco a
@@ -42,19 +43,3 @@ dwn = oct (-1)
 p' = up . p                             -- нота на октаву выше и ниже
 p_ = dwn . p
 
-----------------------------------------------------------------
--- str helpers
-
-en, qn, hn, bn :: Sco a -> Sco a
-enr, qnr, hnr, bnr ::  Sco a
-
-en = str 0.125  -- eights note
-qn = str 0.25   -- quater note
-hn = str 0.5    -- half note
-bn = str 2      -- brewis note (two whole notes
-
--- rests
-enr = rest 0.125  -- eights note
-qnr = rest 0.25   -- quater note
-hnr = rest 0.5    -- half note
-bnr = rest 2      -- brewis note (two whole notes))
